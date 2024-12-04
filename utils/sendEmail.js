57205c29d61a799acc03e8c499fc2ad4c4cecf22
Mostak-Ahamed-Nishat <mostakahamed484@gmail.com>
2024-12-04 21:36:44 +0600

    Update env

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,19 +4,19 @@ import dotenv from "dotenv";
 dotenv.config();
 
 let transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com", // Replace with your SMTP server
-  port: 587, // Replace with the port your SMTP server uses (typically 587 for TLS, 465 for SSL)
+  host: process.env.SMTP_HOST, 
+  port: process.env.SMTP_PORT, 
   secure: false, // true for 465, false for other ports (set to true if using SSL)
   auth: {
-    user: "81035b001@smtp-brevo.com", // Replace with your email
-    pass: "32AnwybtVXEMTgNh", // Replace with your password
+    user: process.env.SMTP_USER, // Replace with your email
+    pass: process.env.SMTP_PASS, // Replace with your password
   },
 });
 
 // Email message options
 let mailOptions = (to, subject, text) => {
   return {
-    from: "mostak35-1952@diu.edu.bd",
+    from: process.env.SMTP_FROM_EMAIL,
     to: to,
     subject: subject, // Subject line
     text: text, // Plain text body
